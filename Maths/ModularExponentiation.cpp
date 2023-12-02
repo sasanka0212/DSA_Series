65c34f717d7db86ce08ac70eb57ec9d1fa31f6ae
sasanka <sasankak2002@gmail.com>
2023-12-02 19:55:54 +0530

    commit

---
+++ Maths/ModularExponentiation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int moduleExponentiation(int a, int b, int m) {
+    long long res = 1;
+    long long x = a;
+    while(b > 0) {
+        if(b & 1) //for odd case
+        {
+            res = (res * (x % m)) % m;
+        }
+        x = (x % m) * (x % m) % m;
+        b = b>>1;
+    }
+    return res;
+}
+
+int main() {
+    //modular exponentiation
+    int a, b, m;
+    cout<<"Enter value, power & mod value : ";
+    cin>>a>>b>>m;
+    cout<<"Value is : "<<moduleExponentiation(a, b, m);
+}
