abc0fd68751a3b81f93dee67b8870cad958ff83e
sasanka <sasankak2002@gmail.com>
2023-12-04 11:49:23 +0530

    commit

---
+++ Maths/PrimeNumberGivenRange.cpp
@@ -0,0 +1,28 @@
+// how many prime numbers in the given range
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Time complexity -> o(n)
+// space complexity -> o(n)
+int primeCount(int num) {
+    int count = 0;
+    vector<int> prime(num + 1, true);
+    prime[0] = prime[1] = false;
+    for(int i = 2; i<num; i++) {
+        if(prime[i]) {
+            count++;
+            for(int n = i*2; n<=num; n = n + i) {
+                prime[n] = false;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int num;
+    cout<<"Enter a number range : ";
+    cin>>num;
+    cout<<"No of prime numbers : "<<primeCount(num);
+}
