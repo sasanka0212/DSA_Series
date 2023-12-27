61a5ebde3ad26c6d243caabadc9a6be86b31fe13
Sasanka <sasankak2002@gmail.com>
2023-12-28 00:45:16 +0530

    commit

---
+++ Recursion/LetterCobinations.cpp
@@ -0,0 +1,36 @@
+// letter combination of a phone keypad number
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void solve(string digits, int index, string output, vector<string> &ans, string nums[]) {
+    if(index == digits.length()) {
+        ans.push_back(output);
+        return;
+    }
+    int n = digits[index] - '0';
+    string value = nums[n];
+    for(int i = 0; i<value.length(); i++) {
+        output.push_back(value[i]);
+        solve(digits, index + 1, output, ans, nums);
+        output.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string digits) {
+    vector<string> ans;
+    string output = "";
+    string nums[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", 
+                    "pqrs", "tuv", "wxyz"};
+    solve(digits, 0, output, ans, nums);
+    return ans;
+}
+
+int main() {
+    string digits = "34";
+    vector<string> ans = letterCombinations(digits);
+    for(string s : ans) {
+        cout<<s<<endl;
+    }
+    return 0;
+}
