510fa4374ebed6d524c9c7579c1cbd9ac30eaade
Sasanka <sasankak2002@gmail.com>
2024-03-03 00:38:38 +0530

    commit

--- Trees/Binary_Trees/binaryTreeImplement.cpp
+++ Trees/Binary_Trees/binaryTreeImplement.cpp
@@ -53,10 +53,81 @@ void levelOrderTraversal(node *root) {
     }
 }
 
+//Insert from level order
+void insertFromLevelOrder(node *&root) {
+    queue<node*> q;
+    int data;
+    cout << "Enter data of root : ";
+    cin >> data;
+    root = (data == -1) ? NULL : new node(data);
+    q.push(root);
+
+    while(!q.empty()) {
+        node *temp = q.front();
+        q.pop();
+
+        if(temp != NULL) {
+            int leftChild;
+            cout << "Enter left child value of " << temp->data << " : ";
+            cin >> leftChild;
+            temp->left = (leftChild == -1) ? NULL : new node(leftChild);
+            q.push(temp->left);
+
+            int rightChild;
+            cout << "Enter right child value of " << temp->data << " : ";
+            cin >> rightChild;
+            temp->right = (rightChild == -1) ? NULL : new node(rightChild);
+            q.push(temp->right);
+        }
+    }
+}
+
+//Inorder Traversal
+void inorderTraversal(node *root) {
+    if(root == NULL) 
+        return;
+
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+//Preorder traversal
+void preorderTraversal(node *root) {
+    if(root == NULL) 
+        return;
+
+    cout << root->data << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+//Postorder Traversal
+void postorderTraversal(node *root) {
+    if(root == NULL) 
+        return;
+    
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->data << " ";
+}
+
 int main() {
     node *root = NULL;
-    root = insertNode(root);
+    //root = insertNode(root);
+    insertFromLevelOrder(root);
+    // 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
 
     // level order traversal
+    cout << endl << "Level order Traversal : " << endl;
     levelOrderTraversal(root);
+
+    cout << endl << "Inorder traversal : " << endl;
+    inorderTraversal(root);
+
+    cout << endl << "Preorder Traversal : " << endl;
+    preorderTraversal(root);
+
+    cout << endl << "Postorder Traversal : " << endl;
+    postorderTraversal(root);
 }
