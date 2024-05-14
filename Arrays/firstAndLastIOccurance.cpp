a32fa6eafdbcff78c84a8386217094af7e21dbf4
Sasanka <sasankak2002@gmail.com>
2024-05-14 11:50:33 +0530

    commit

---
+++ Arrays/firstAndLastIOccurance.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+pair<int, int> findFirstAndLastIndex(vector<int> &arr, int key) {
+    pair<int, int> ans = make_pair(-1, -1);
+
+    int start = 0;
+    int end = arr.size() - 1;
+    //first occurance
+    while(start<=end) {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == key) {
+            ans.first = mid;
+            end = mid - 1;
+        } else if(key > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end  = mid - 1;
+        }
+    }
+
+    start = 0;
+    end = arr.size() - 1;
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == key) {
+            start = mid + 1;
+            ans.second = mid; 
+        } else if(key > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {1, 1, 3, 3, 3, 7, 9, 10};
+
+    pair<int, int> ansIndex = findFirstAndLastIndex(arr, 3);
+    cout << endl << "First Index :" << ansIndex.first << endl;
+    cout << endl << "Last Index :" << ansIndex.second << endl;
+}
